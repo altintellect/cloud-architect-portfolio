5c84a03b6a7c47393bc9fe650e4f70513f15ce3f
Acadian <acadian.eng@gmail.com>
2026-04-16 19:27:07 -0400

    restrict ssh access to my ip only

--- iac/azdemo/main.tf
+++ iac/azdemo/main.tf
@@ -82,7 +82,7 @@ resource "azurerm_network_security_group" "azdemo" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = "99.228.241.129/32"
     destination_address_prefix = "*"
   }
 
